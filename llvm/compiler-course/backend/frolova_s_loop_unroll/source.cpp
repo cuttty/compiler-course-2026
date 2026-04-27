09719455bc3c8e46585f874f3a7095e92fa1c317
cuttty <frolova.sofya.05@mail.ru>
2026-04-27 14:50:05 +0300

    fix clang-format

--- llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
+++ llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
@@ -10,7 +10,7 @@
 using namespace llvm;
 
 namespace {
-class FrolovaSLoopUnroll: public MachineFunctionPass {
+class FrolovaSLoopUnroll : public MachineFunctionPass {
 public:
   static char ID;
   FrolovaSLoopUnroll() : MachineFunctionPass(ID) {}
@@ -23,13 +23,16 @@ public:
   bool runOnMachineFunction(MachineFunction &MF) override;
 
 private:
-  bool processLoop(MachineLoop *L, MachineFunction &MF, const TargetInstrInfo *TII);
-  bool unrollLoop(MachineLoop *L, MachineFunction &MF, const TargetInstrInfo *TII, unsigned UnrollCount);
+  bool processLoop(MachineLoop *L, MachineFunction &MF,
+                   const TargetInstrInfo *TII);
+  bool unrollLoop(MachineLoop *L, MachineFunction &MF,
+                  const TargetInstrInfo *TII, unsigned UnrollCount);
 };
 
 char FrolovaSLoopUnroll::ID = 0;
 
-bool FrolovaSLoopUnroll::processLoop(MachineLoop *L, MachineFunction &MF, const TargetInstrInfo *TII) {
+bool FrolovaSLoopUnroll::processLoop(MachineLoop *L, MachineFunction &MF,
+                                     const TargetInstrInfo *TII) {
   bool Changed = false;
 
   for (MachineLoop *InnerLoop : *L) {
@@ -37,27 +40,29 @@ bool FrolovaSLoopUnroll::processLoop(MachineLoop *L, MachineFunction &MF, const 
   }
 
   unsigned MaxUnrollFactor = 5;
-  unsigned UnrollCount = MaxUnrollFactor; 
+  unsigned UnrollCount = MaxUnrollFactor;
 
   Changed |= unrollLoop(L, MF, TII, UnrollCount);
 
   return Changed;
 }
 
-bool FrolovaSLoopUnroll::unrollLoop(MachineLoop *L, MachineFunction &MF, const TargetInstrInfo *TII, unsigned UnrollCount) {
-  if (UnrollCount <= 1) return false;
+bool FrolovaSLoopUnroll::unrollLoop(MachineLoop *L, MachineFunction &MF,
+                                    const TargetInstrInfo *TII,
+                                    unsigned UnrollCount) {
+  if (UnrollCount <= 1)
+    return false;
 
   MachineBasicBlock *LoopMBB = L->getHeader();
   if (L->getNumBlocks() != 1) {
-    llvm::outs() << "Skipping complex loop (multiple blocks) in " << MF.getName() << "\n";
+    llvm::outs() << "Skipping complex loop (multiple blocks) in "
+                 << MF.getName() << "\n";
     return false;
   }
 
-  llvm::outs() << "Unrolling loop in " << MF.getName() 
+  llvm::outs() << "Unrolling loop in " << MF.getName()
                << " (Factor: " << UnrollCount << ")\n";
 
-  MachineRegisterInfo &MRI = MF.getRegInfo();
-  
   SmallVector<MachineInstr *, 8> InstrsToClone;
   for (MachineInstr &MI : *LoopMBB) {
     if (!MI.isBranch() && !MI.isTerminator()) {
@@ -73,16 +78,17 @@ bool FrolovaSLoopUnroll::unrollLoop(MachineLoop *L, MachineFunction &MF, const T
       LoopMBB->insert(InsertPos, ClonedMI);
     }
   }
-  
+
   return true;
 }
 
 bool FrolovaSLoopUnroll::runOnMachineFunction(MachineFunction &MF) {
-  llvm::outs() << "Running ExamplePass on function: " << MF.getName() << '\n';
-  
+  llvm::outs() << "Running FrolovaSLoopUnroll on function: " << MF.getName()
+               << '\n';
+
   MachineLoopInfo &MLI = getAnalysis<MachineLoopInfo>();
   const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
-  
+
   bool Changed = false;
 
   for (MachineLoop *L : MLI) {
@@ -93,5 +99,6 @@ bool FrolovaSLoopUnroll::runOnMachineFunction(MachineFunction &MF) {
 }
 } // namespace
 
-static RegisterPass<ExamplePass> X("example-x86", "FrolovaSLoopUnrollPass", false, false);
+static RegisterPass<FrolovaSLoopUnroll> X("example-x86", "FrolovaSLoopUnrollPass",
+                                          false, false);
 
