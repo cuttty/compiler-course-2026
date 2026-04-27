9cea6028c598841407a23d346c9c5a70d499f90f
cuttty <frolova.sofya.05@mail.ru>
2026-04-27 15:18:36 +0300

    delete extra enter

--- llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
+++ llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
@@ -101,4 +101,4 @@ bool FrolovaSLoopUnroll::runOnMachineFunction(MachineFunction &MF) {
 
 static RegisterPass<FrolovaSLoopUnroll>
     X("example-x86", "FrolovaSLoopUnrollPass", false, false);
-    
+
