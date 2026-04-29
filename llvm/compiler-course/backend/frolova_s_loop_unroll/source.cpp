c4e7b7019790f08c16f65059a27c6b074cdcb62b
cuttty <frolova.sofya.05@mail.ru>
2026-04-29 21:53:54 +0300

    fix

--- llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
+++ llvm/compiler-course/backend/frolova_s_loop_unroll/source.cpp
@@ -100,5 +100,4 @@ bool FrolovaSLoopUnroll::runOnMachineFunction(MachineFunction &MF) {
 
 static RegisterPass<FrolovaSLoopUnroll>
     X("example-x86", "FrolovaSLoopUnrollPass", false, false);
-
     
